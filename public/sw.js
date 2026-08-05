86dd1d292d464a2e55cfa1104c9fc811ad612719
eudsonalmeida-afk <eudson.almeida@gmail.com>
2026-08-05 05:24:20 -0300

    Renovar cache do PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "biolab-pwa-v1";
+const CACHE_NAME = "biolab-pwa-v2";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./icons/biolab-icon.png", "./og.png"];
 
 self.addEventListener("install", (event) => {
